51e2ae779f1d795dce85893d0126f8887011a23d
DevinRobertsdirect <your.email@example.com>
2025-09-24 12:03:43 -0400

    Add Firebase configuration and Sports screen; fix critical navigation issues

---
+++ config/firebase.ts
@@ -0,0 +1,19 @@
+﻿import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDARuAmiDKTAOtwRR3VMwFd__ehGkRXypw',
+  authDomain: 'gymni-fitness.firebaseapp.com',
+  projectId: 'gymni-fitness',
+  storageBucket: 'gymni-fitness.appspot.com',
+  messagingSenderId: '796288733107',
+  appId: '1:796288733107:web:99e83c111ad841d8a605c6',
+  measurementId: 'G-J7FY0W6ZC8'
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
